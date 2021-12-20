11eb74a50bacc77c63905b5b0b9f09fa7a0d7335
Petro Arva <petroarva33@gmail.com>
2021-12-20 21:22:20 +0100

    Basic files like app.js index.js, file for data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import { feedbackData } from "./data/FeedbackData";
+
 function App() {
-  return <h1>Hello from the app component</h1>;
+  const [feedback, setFeedback] = useState(feedbackData);
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackList feedback={feedback} />
+      </div>
+    </>
+  );
 }
 
 export default App;
