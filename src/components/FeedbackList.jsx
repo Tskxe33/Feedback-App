d02061fca537130c732a78acce1c4eee93b8c987
Petro Arva <petroarva33@gmail.com>
2021-12-21 16:39:19 +0100

    Added propTypes

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import FeedbackItem from "./FeedbackItem";
 
 function FeedbackList({ feedback }) {
@@ -12,5 +13,9 @@ function FeedbackList({ feedback }) {
     </div>
   );
 }
+
+FeedbackList.propTypes = {
+  feedback: PropTypes.array,
+};
 
 export default FeedbackList;
