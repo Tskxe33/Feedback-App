9330adde7d04500468b9d9c60b789bc365a31b6a
Petro Arva <petroarva33@gmail.com>
2021-12-23 20:26:57 +0100

    Imported form and stats components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import { feedbackData } from "./data/FeedbackData";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
   const [feedback, setFeedback] = useState(feedbackData);
@@ -16,6 +18,8 @@ function App() {
     <>
       <Header />
       <div className="container">
+        <FeedbackForm />
+        <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
     </>
