31d47254a985f17750c656fe9cdfe7b31b992594
Petro Arva <petroarva33@gmail.com>
2021-12-23 20:27:25 +0100

    Created form and stats components

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Button from "./shared/Button";
+import Card from "./shared/Card";
+
+function FeedbackForm() {
+  const [text, setText] = useState("");
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState("");
+
+  const handleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage("Text must be at least 10 characters");
+    } else {
+      setMessage(null);
+      setBtnDisabled(false);
+    }
+
+    setText(e.target.value);
+  };
+  return (
+    <Card>
+      <form>
+        <h2>How would you rate your service with us?</h2>
+        {/* @todo - rating select component */}
+
+        <div className="input-group">
+          <input
+            type="text"
+            onChange={handleTextChange}
+            placeholder="Write a review"
+            value={text}
+          />
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
+        </div>
+        {message && <div className="message">{message}</div>}
+      </form>
+    </Card>
+  );
+}
+
+export default FeedbackForm;
