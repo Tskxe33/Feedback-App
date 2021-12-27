c2e734a920fcb0104cc7b400412f7c2b3cf3aa8b
Petro Arva <petroarva33@gmail.com>
2021-12-27 10:25:45 +0100

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { v4 as uuidv4 } from "uuid";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import { feedbackData } from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
+import AboutIconLink from "./components/AboutIconLink";
+import Post from "./components/Post";
 
 function App() {
   const [feedback, setFeedback] = useState(feedbackData);
@@ -21,14 +25,31 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <Header />
-      <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-      </div>
-    </>
+
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="container">
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </div>
+            </>
+          }
+        ></Route>
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/post/:id/:name" element={<Post />} />
+      </Routes>
+
+      <AboutIconLink />
+    </Router>
   );
 }
 
